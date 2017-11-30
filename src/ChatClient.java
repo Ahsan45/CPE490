30f6b3a91c4272b819bb5856aaaac2ebf4f360a2
Ahsan45 <ashahab@stevens.edu>
2017-11-30 14:51:38 -0500

    Add files via upload

---
+++ src/ChatClient.java
@@ -0,0 +1,105 @@
+import java.awt.event.*;
+import java.io.*;
+import java.net.Socket;
+import javax.swing.*;
+
+public class ChatClient {
+
+    BufferedReader in;
+    PrintWriter out;
+    JFrame frame = new JFrame("IRC Chat Project");
+    JTextField textField = new JTextField(100);
+    JTextArea messageArea = new JTextArea(50, 100);
+
+    /**
+     * Constructs the client by laying out the GUI and registering a
+     * listener with the textfield so that pressing Return in the
+     * listener sends the textfield contents to the server.  Note
+     * however that the textfield is initially NOT editable, and
+     * only becomes editable AFTER the client receives the NAMEACCEPTED
+     * message from the server.
+     */
+    public ChatClient() {
+
+        // Layout GUI
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        frame.pack();
+
+        // Add Listeners
+        textField.addActionListener(new ActionListener() {
+            /**
+             * Responds to pressing the enter key in the textfield by sending
+             * the contents of the text field to the server.    Then clear
+             * the text area in preparation for the next message.
+             */
+            public void actionPerformed(ActionEvent e) {
+                out.println(textField.getText());
+                textField.setText("");
+            }
+        });
+    }
+
+    /**
+     * Prompt for and return the address of the server.
+     */
+    private String getServerAddress() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Enter IP Address of the Server:",
+            "Welcome to the Chatter",
+            JOptionPane.QUESTION_MESSAGE);
+    }
+
+    /**
+     * Prompt for and return the desired screen name.
+     */
+    private String getName() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Choose a screen name:",
+            "Screen name selection",
+            JOptionPane.PLAIN_MESSAGE);
+    }
+
+    /**
+     * Connects to the server then enters the processing loop.
+     */
+    private void run() throws IOException {
+
+        // Make connection and initialize streams
+        String serverAddress = getServerAddress();
+        Socket socket = new Socket(serverAddress, 9001);
+        in = new BufferedReader(new InputStreamReader(
+            socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Process all messages from server, according to the protocol.
+        while (true) {
+            String line = in.readLine();
+            if (line.startsWith("SUBMITNAME")) {
+                out.println(getName());
+            } else if (line.startsWith("NAMEACCEPTED")) {
+                textField.setEditable(true);
+            } else if (line.startsWith("MESSAGE")) {
+                messageArea.append(line.substring(8) + "\n");
+            }
+            else {
+            	break;
+            }
+        }
+        socket.close();
+    }
+
+    /**
+     * Runs the client as an application with a closeable frame.
+     */
+    public static void main(String[] args) throws Exception {
+        ChatClient client = new ChatClient();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
+    }
+}
