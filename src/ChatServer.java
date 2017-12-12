04bab3f6ee13cbea9bd9f7dda4097ab0122ac0d4
tasteinberg <34046098+tasteinberg@users.noreply.github.com>
2017-12-11 21:09:22 -0500

    Update ChatServer.java

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -70,7 +70,7 @@ public class ChatServer {
                 out.println("NAMEACCEPTED");
                 writers.add(out);
                 for (PrintWriter writer : writers) {
-                    writer.println("MESSAGE " + name + " has joined. Type /help for help.");
+                    writer.println("SERVER " + "[" + LocalTime.of(LocalTime.now().getHour(), LocalTime.now().getMinute()) + "]" + " " + name + " has joined. Type /help for help.");
                 }
 
                 // Accept messages from this client and broadcast them.
